b143b388ab422b7ad5d3374d65f3e961912412f0
shawnhoefling <shawnhoefling@gmail.com>
2024-10-30 16:54:21 +0200

    Adding Drawing class, fixes to other classes and one interface Visibility

---
+++ Drawing.java
@@ -0,0 +1,12 @@
+import java.util.List;
+import java.util.ArrayList;
+
+public class Drawing {
+    private List<T> = new ArrayList<>();
+    public Drawing() {
+        // constructor
+    }
+    void addBasicShape(T);
+    List<T> getBasicShapes();
+    void draw();
+}
