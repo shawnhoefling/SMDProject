8e2417d07ed22d0ea3912c1ef18bbdc2fdfa9cb2
shawnhoefling <shawnhoefling@gmail.com>
2024-10-30 16:45:41 +0200

    Adding Rectangle interface class

---
+++ Rectangle.java
@@ -0,0 +1,15 @@
+public class Rectangle {
+    public Rectangle{
+        // constructor
+    }
+    void draw();
+    void grow();
+    String printText();
+}
+/*
+ Add the required content and functionality shown in the class diagram above and following the instructions below.
+
+The method draw() must output the text "This is a rectangle" (not actually drawing anything).
+The method grow() must output the text "Growing the size of the rectangle" (not actually resizing anything).
+The method printText() must return the text "Rectangle".
+ */
