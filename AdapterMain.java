e82071ba1b9362a8bf53e7f8836b7443e4d2742e
shawnhoefling <shawnhoefling@gmail.com>
2024-10-30 17:00:07 +0200

    adding Adapter Main, main class of the program

---
+++ AdapterMain.java
@@ -0,0 +1,51 @@
+public class AdapterMain {
+    public static void main(String[] args) {
+        
+    }
+    
+}
+
+/*
+ Add the required content and functionality shown in the class diagram & following instructions.
+
+
+In the main method you must...
+
+Create a ShapeDrawer object to be your drawing object (like a canvas where you add objects)
+Create a few objcets (as given below) and add those objects to your drawing...
+Add a rectangle which is visible
+Add a rectangle which is in the background
+Add a circle which is visible
+Add a circle which is in the background
+Add a rectangle which is visible
+Draw your drawing (call the method draw() on it, not on individual objects created in the main method)
+Grow the size of your drawing (call the method grow() on it, not on individual objects created in the main method)
+Print all shapes in your drawing by calling the method showVisibility() on your drawing. 
+The output for each shape (included in the drawing) will include its order number in the drawing, information whether the shape is visible or not, and the type of the shape (see example output below). Finally, the number of objects in the drawing will be shown.
+
+
+So, the main method is rather short in itself. Study the instructions with care! After implementing all required classes and the main method, test your program.
+
+After testing, continue with the changes for the phase 2...
+
+
+Expected output from the program (Phase 1)
+
+    This is a rectangle
+    This is a rectangle
+    This is a circle
+    This is a circle
+    This is a rectangle
+    Growing the size of the rectangle
+    Growing the size of the rectangle
+    Growing the size of the circle
+    Growing the size of the circle
+    Growing the size of the rectangle
+
+    1. shape is visible: Rectangle
+    2. shape is in the background: Rectangle
+    3. shape is visible: Circle
+    4. shape is in the background: Circle
+    5. shape is visible: Rectangle
+    Total number of shapes: 5
+ */
