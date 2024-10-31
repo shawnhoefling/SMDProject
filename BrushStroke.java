7c7b38b74f5edd643cb424653b4fe16b8785ceff
shawnhoefling <shawnhoefling@gmail.com>
2024-10-31 08:16:30 +0200

    Finished implementation of BrushStroke.java

--- BrushStroke.java
+++ BrushStroke.java
@@ -1,39 +1,49 @@
 import java.util.Random;
 
-public class BrushStroke {
+public class BrushStroke extends Visibility implements ShapeBasic, CanBeColored {
+    private int thickness; // change 1 to the random variable from constructor
+    private String color = "Yellow";
     Random random = new Random();
-    int thickness = 0; // change 1 to the random variable from constructor
-    String color = "Yellow";
     
 
     public BrushStroke() { // fix this, ensure thickness is properly set
-        
-        int thickness = random.nextInt(10) + 1;
+        this.thickness = random.nextInt(10, 1); // check bounds here
     }
     
     @Override
     public void draw() {
+        System.out.println("Brush strokes with thickness " + thickness); //ensure correct thickness being calculated
 
     }
 
     @Override
     public void grow() {
+        System.out.println("Adding some color... " + getColor());
 
     }
 
     @Override
     public String printText(){
+        return "Brush Stroke";
 
     }
 
     @Override
-    default String setColor(){
-
+    public String setColor(String color){
+        String validatedColor = CanBeColored.super.setColor(color); // super to access interface directly, uses default implementation of setColor in CanBeColored
+        this.color = validatedColor;
+        return validatedColor;
     }
 
     @Override
     public String getColor(){
-        
+        if("No color".equals(this.color)) {
+            return "Out of color...";
+        }
+        else {
+            return this.color;
+        }
+
     }
 
 }
