65c7b2b81b8383bea65d412a78db862a8a9b09b7
shawnhoefling <shawnhoefling@gmail.com>
2024-10-31 06:38:17 +0200

    Adding implementation of ShapeAdapter, waiting for Triangle and ShapeDiamond to complete implementation

---
+++ ShapeAdapter.java
@@ -0,0 +1,31 @@
+public class ShapeAdapter extends Visibility implements ShapeBasic {
+    private ShapeSpecial shapeSpec; // 
+    ShapeAdapter(ShapeSpecial shapeSpec){ // constructor 
+        this.shapeSpec = shapeSpec; // 
+    }
+    @Override // overriding as its overriding an interface
+    public void draw(){ // draw calls drawshape method on shapeSpec object
+        shapeSpec.drawShape();
+    }
+
+    @Override
+    public void grow(){ // uses printText() to return shape and add cannot grow
+        System.out.println(printText() + ", cannot grow");
+    }
+
+    @Override
+    public String printText(){  // using instanceof to ensure its correct class 
+        if (shapeSpec instanceof Triangle) { // Triangle class not implemented yet
+            return "Triangle";
+        }
+        else if (shapeSpec instanceof ShapeDiamond) { // Diamond class not implemented yet
+            return "ShapeDiamond";
+
+        }
+        else {
+            System.out.println("This shape is not supported");
+        }
+
+    }
+    
+}
